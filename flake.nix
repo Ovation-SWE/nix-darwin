9e927c43168cc7c281457c2c2eae4bd70417b367
Ovation <reach-gaurav-joshi@protonmail.com>
2026-06-30 08:29:23 -0600

    refactor: recursive module discovery, reorganize into subdirectories

--- flake.nix
+++ flake.nix
@@ -5,31 +5,30 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    
-    # ADD THESE TWO LINES 👇
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ self, flake-parts, nix-darwin, nixpkgs, ... }:
+  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
     let
-      # Automatically reads the modules directory and generates an import list
-      autoModules = let
-        dirContents = builtins.readDir ./modules;
-        validFiles = nixpkgs.lib.filterAttrs (name: type:
-          (type == "regular" || type == "symlink") &&
-          nixpkgs.lib.hasSuffix ".nix" name &&
-          name != "hosts.nix" # Exclude the host definition tracker itself
-        ) dirContents;
-      in
-        map (name: ./modules + "/${name}") (builtins.attrNames validFiles);
+      collectNixFiles = dir:
+        let
+          contents = builtins.readDir dir;
+        in
+          builtins.concatLists (builtins.attrValues (
+            builtins.mapAttrs (name: type:
+              if type == "directory"
+              then collectNixFiles (dir + "/${name}")
+              else if type == "regular" && nixpkgs.lib.hasSuffix ".nix" name
+              then [ (dir + "/${name}") ]
+              else []
+            ) contents
+          ));
     in
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.flake-parts.flakeModules.modules
-        ./modules/hosts.nix # Statically import our machine composition file
-      ] ++ autoModules;      # Dynamically import all other feature modules
+      ] ++ collectNixFiles ./modules;
     };
 }
